0f52ceefd8c6f97fadc7fd8d8f4a7b921d1106d6
Jiří Novák <jiri.novak@keboola.com>
2024-10-31 09:53:28 +0100

    Add dump of config.toml for debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,37 @@
 import streamlit as st
+import toml
+import os
 
-st.title("Hello World App")
+tabs = st.tabs(["Hello world", "Debug"])
 
-st.write("Hello, world! Toto je tvoje první aplikace ve Streamlit.")
+with tabs[0]:
+    st.header("Hello World App")
+    st.write("Hello, world! Toto je tvoje první aplikace ve Streamlit.")
 
-name = st.text_input("Zadej své jméno:")
+    name = st.text_input("Zadej své jméno:")
 
-if st.button("Pozdrav"):
-    st.write(f"Ahoj, {name}!")
+    if st.button("Pozdrav"):
+        st.write(f"Ahoj, {name}!")
+
+with tabs[1]:
+    st.header("Debug")
+
+    config_file_path = os.path.join(os.path.dirname(__file__), os.path.pardir, '.streamlit', 'config.toml')
+
+    try:
+        config = toml.load(config_file_path)
+
+        st.title('Current path')
+        st.write(os.path.dirname(__file__))
+
+        st.title('Current working directory')
+        st.write(os.getcwd())
+
+        st.title('Obsah config.toml')
+        st.write(config)
+
+    except FileNotFoundError:
+        st.error(f'Soubor {config_file_path} nebyl nalezen.')
+    except toml.TomlDecodeError:
+        st.error(f'Soubor {config_file_path} má neplatný formát.')
+
